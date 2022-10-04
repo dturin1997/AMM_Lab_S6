26c44df826fd9dfd46d1136a7a2e0b14e0117f4b
Dario Turin <dturin1997@gmail.com>
2022-10-04 15:02:58 -0500

    Agregar archivos de proyecto.

---
+++ AMM_Lab_S6/AMM_Lab_S6/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AMM_Lab_S6
+{
+    public class Employee
+    {
+        public string DisplayName { get; set; }
+    }
+}
---
+++ AMM_Lab_S6/AMM_Lab_S6/PageModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AMM_Lab_S6
+{
+    public class PageModel
+    {
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public PageModel(string title, string subtitle)
+        {
+            Title = title;
+            Subtitle = subtitle;
+        }
+    }
+}
